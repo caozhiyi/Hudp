fdf9d585d4a3c43426fd935772984bb4e3293ffb
caozhiyi <caozhiyi5@gmail.com>
2020-03-20 20:40:47 +0800

    add socket status.

--- common/CommonFlag.h
+++ common/CommonFlag.h
@@ -21,9 +21,9 @@ namespace hudp {
         HPF_RELIABLE_ORDERLY_ACK_RANGE = 0x000200,
 
         // to close connection
-        HPF_CLOSE                      = 0x000400,
+        HPF_FIN                        = 0x000400,
         // close ack
-        HPF_CLOSE_ACK                  = 0x000800,
+        HPF_FIN_ACK                    = 0x000800,
 
         // bag has incremental id.
         HPF_WITH_ID                    = 0x001000,
@@ -35,6 +35,51 @@ namespace hudp {
 
         // msg with send time stamp, to calculation rtt time
         HPF_MSG_WITH_TIME_STAMP        = 0x010000,
+    };
+
+    /*
+    * socket status when hudp running.
+    * SS_INIT: when a socket just created.
+    * SS_READY: client send a mag to remote and recv a ack back
+    *           or server recv a msg. 
+    *
+    * The following states are the same as TCP.
+    * SS_FIN_WAIT_1: when Close be called, send all msg in cache then send HPF_FIN to remote
+    * SS_FIN_WAIT_2: when SS_FIN_WAIT_1 recv a HPF_FIN_ACK
+    * SS_TIMW_WAIT: when SS_FIN_WAIT_2 recv a HPF_FIN
+    * SS_CLOSE_WIAT: when SS_READY recv a HPF_FIN, send all msg in cache the send HPF_FIN to remote
+    * SS_LAST_ACK: when SS_CLOSE_WIAT send HPF_FIN to remote until recv HPF_FIN_ACK
+    *
+    * status change:
+    *     client         server
+    *              MSG
+    *     SS_INIT------->SS_INIT
+    *       |               |
+    *       V      ACK      V
+    *    SS_READY<-------SS_READY
+    *       
+    *              FIN
+    *    SS_READY------->SS_READY
+    *       |               |
+    *       V               V
+    * SS_FIN_WAIT_1    SS_CLOSE_WIAT
+    *       |
+    *       V     FIN_ACK
+    * SS_FIN_WAIT_2<---SS_CLOSE_WIAT
+    *                       |
+    *               FIN     V
+    *  SS_TIME_WAIT<---SS_LAST_ACK
+    *             FIN_ACK
+    *  SS_TIME_WAIT--->SS_LAST_ACK
+    */
+    enum socket_status {
+        SS_INIT       = 0,
+        SS_READY      = 1,
+        SS_FIN_WAIT_1 = 2,
+        SS_FIN_WAIT_2 = 3,
+        SS_TIME_WAIT  = 4,
+        SS_CLOSE_WIAT = 5,
+        SS_LAST_ACK   = 6
     };
 }
 #endif
