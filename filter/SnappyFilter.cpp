a3d5805c6e233a3079f208335bbdb59168cfad39
caozhiyi <caozhiyi5@gmail.com>
2020-03-26 20:55:16 +0800

    add snappy filter.

---
+++ filter/SnappyFilter.cpp
@@ -0,0 +1,46 @@
+#include "Log.h"
+#include "snappy-c.h"
+#include "HudpImpl.h"
+#include "SnappyFilter.h"
+
+using namespace hudp;
+
+bool CSnappyFilter::FilterProcess(const HudpHandle& handle, uint16_t flag, std::string& body) {
+    std::string send_msg;
+    SnappyCompress(body, send_msg);
+    return _next_filter->FilterProcess(handle, flag, send_msg);
+}
+
+bool CSnappyFilter::RelieveFilterProcess(const HudpHandle& handle, uint16_t flag, std::string& body) {
+    std::string recv_msg;
+    SnappyUncompress(body, recv_msg);
+    return _next_filter->FilterProcess(handle, flag, recv_msg);
+}
+
+bool CSnappyFilter::SnappyCompress(std::string &in_put, std::string &output) {
+    char* input_pt = (char*)in_put.data();
+    size_t out_size = snappy_max_compressed_length(in_put.size());
+
+    output.clear();
+    output.resize(out_size);
+    if (snappy_compress(input_pt, in_put.size(), (char*)&*output.begin(), &out_size) != SNAPPY_OK) {
+        base::LOG_ERROR("compress failed.");
+        return false;
+    }
+    return true;
+}
+
+bool CSnappyFilter::SnappyUncompress(std::string &in_put, std::string &output) {
+    size_t out_size = 0;
+    if (snappy_uncompressed_length(in_put.data(), in_put.length(), &out_size) != SNAPPY_OK) {
+        base::LOG_ERROR("get out_put size failed");
+        return false;
+    }
+    output.clear();
+    output.resize(out_size);
+    if (snappy_uncompress(in_put.data(), in_put.length(), (char*)&*output.begin(), &out_size) != SNAPPY_OK) {
+        base::LOG_ERROR(" snappy uncompress failed");
+        return false;
+    }
+    return true;
+}
---
+++ filter/SnappyFilter.h
@@ -0,0 +1,23 @@
+#ifndef HEADER_FILTER_SNAPPY_FILTER
+#define HEADER_FILTER_SNAPPY_FILTER
+
+#include "IFilter.h"
+
+namespace hudp {
+    // the firt filter
+    // send msg to upper
+    class CSnappyFilter : public CFilter {
+    public:
+        CSnappyFilter() {}
+        virtual ~CSnappyFilter() {}
+        // when send msg filter process
+        bool FilterProcess(const HudpHandle& handle, uint16_t flag, std::string& body);
+        // when recv msg filter process
+        bool RelieveFilterProcess(const HudpHandle& handle, uint16_t flag, std::string& body);
+
+    private:
+        bool SnappyCompress(std::string &in_put, std::string &out_put);
+        bool SnappyUncompress(std::string &in_put, std::string &output);
+    };
+}
+#endif
